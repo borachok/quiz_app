25eba36935637b1228f0e9090dcd093ba8c8d0d6
Maxim_Borachok <maxim_borachok@epam.com>
2021-03-18 11:29:46 +0200

    add-app

---
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,46 @@
+import React from "react";
+import classes from './FinishedQuiz.module.css'
+import Button from "../UI/Button/Button";
+
+const FinishedQuiz = props => {
+  const successCount =  Object.keys(props.results).reduce((total,key)=> {
+    if (props.results[key] === 'success'){
+      total++
+    }
+
+    return total
+  },0)
+
+  return (
+    <div className={classes.FinishedQuiz}>
+        <ul>
+          {
+            props.quiz.map((quizItem, index) => {
+              const cls = [
+                'fa',
+                props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                classes[props.results[quizItem.id]]
+              ]
+
+              return (
+                <li key={index}>
+                  <strong>{index + 1}</strong>.&nbsp;
+                  {quizItem.question}
+                  <i className={cls.join(' ')}/>
+                </li>
+              )
+            })
+          }
+        </ul>
+
+      <p>Correct: {successCount} / {props.quiz.length}</p>
+
+      <div>
+        <Button onClick={props.onRetry} type='primary'>Reply</Button>
+        <Button onClick={props.onRetry} type='success'>Go to List Tests</Button>
+      </div>
+    </div>
+  )
+}
+
+export default FinishedQuiz;
